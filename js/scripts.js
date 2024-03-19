6a953a92340dfb085da5387b9589b317bcaa637d
Andrew Kelly <andrewklly@protonmail.com>
2024-03-19 07:32:07 -0500

    not working...

--- js/scripts.js
+++ js/scripts.js
@@ -218,7 +218,7 @@ $(document).ready(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbyy4B0JF82jxSfmje6G4DsdNeZxmLa3c9BOCAazyVALfJuljfVv-_DfYhKG1tmfmgIhnQ/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbySIf472AoYtc405HZ3woJ90n1l2S9Q4n5dkpmsa_ybbnfxcbC-X8jN6_fa4MOkuznL6g/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
