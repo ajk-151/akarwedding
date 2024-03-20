165dab3d4b1b6613fea87d00b377933934bb1988
Andrew Kelly <andrewklly@protonmail.com>
2024-03-20 05:02:38 -0500

    updated endpoint

--- js/scripts.js
+++ js/scripts.js
@@ -214,11 +214,11 @@ $(document).ready(function () {
 
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
 
-        if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
-            && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
+        if (MD5($('#invite_code').val()) !== 'b7fbf2c3031b1ee129dda1105fa1f4a7'
+            && MD5($('#invite_code').val()) !== 'b7fbf2c3031b1ee129dda1105fa1f4a7') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbyHpWOYVSudIA-I5fT8G4hrrjgTtgX_r9Z5yhwjzshIfPfGr8l7OdNlypkHL-atAfL-cA/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbxBQ6jruN21w0W2uJWuYvcWJW9mWCi1sU4zhKqXxCmwnW8noxUPGZSJlWg_Z0dPFXX9Ow/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
